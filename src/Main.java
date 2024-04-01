05a16d4f339c871e6ebda08767941129e099d926
Ян <peroff007@gmail.com>
2024-04-02 01:30:24 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1:");
         String firstName = "Ivan";
-        String secondName = "Ivanov";
-        String lastName = "Ivanovich";
-        String fullName = secondName + " " + firstName + " " + lastName;
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        firstName = firstName.replace(" ", "");
+        middleName = middleName.replace(" ", "");
+        lastName = lastName.replace(" ", "");
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println(" ");
+        System.out.println("Задание 2:");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
     }
 }
